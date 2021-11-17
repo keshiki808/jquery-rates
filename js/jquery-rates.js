91a2cac0f6530db5bdc181c0701274fbc9c69bf4
keshiki808 <keshiki808@gmail.com>
2021-11-17 18:47:45 -0330

    Added hover feature and score object

--- js/jquery-rates.js
+++ js/jquery-rates.js
@@ -5,6 +5,13 @@
 
         let $imageStar; 
         let $container = this;
+        const score = {
+          value: 0
+        }
+
+        console.log(score.value)
+
+
         
 
         createStars($container);
@@ -12,11 +19,40 @@
         createStars($container);
         createStars($container);
         createStars($container);
+        $(this).find('img').hover(function(){
+          
+          let starIndex = ($('img').index(this));
 
-        $(this).find('img').on('click', function(container){
+          for (let i = 0; i<=starIndex; i++){
+            console.log('hover in')
+            let star = ($('img').get(i))
+            $(star).addClass('colored')
+          }
+          }, function(){
+          console.log('hover out')
+          let starTotal = $($container).find('img').length
+          console.log(starTotal)
+          console.log(score.value)
+          for (let j = score.value ; j<starTotal; j++){
+            console.log('hover out')
+            let star = ($('img').get(j))
+            $(star).removeClass('colored')
+          }
+        })
+
+        
+
+        $(this).find('img').on('click', function(){
           // console.log($('$container').find(img))
           console.log($('container img'))
-
+          let starIndex = ($('img').index(this));
+          console.log(starIndex)
+          $('img').removeClass('colored')
+          for (let i = 0; i<= starIndex; i++){
+            let star = ($('img').get(i))
+            $(star).addClass('colored')
+          }
+          score.value = starIndex + 1
         })
 
         function createStars(container){
@@ -26,10 +62,6 @@
           $(container).append($imageStar)
           
         }
-
-
-
-          
 
       });
 
