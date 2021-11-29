91c38c792f3e5dc8de00fcbc3f73b7f68544da25
keshiki808 <keshiki808@gmail.com>
2021-11-29 20:01:27 -0330

    Modifed hover and shading mechanics, slight code reformat

--- js/jquery-rates.js
+++ js/jquery-rates.js
@@ -1,79 +1,77 @@
 (function ($) {
   $.fn.rates = function (options) {
-    //Default settings for the plugin if none are provided by the user
+    // Default settings for the plugin if none are provided by the user
     const settings = $.extend({
-      shadeColor: 'brightness(0) saturate(100%) invert(89%) sepia(82%) saturate(5150%) hue-rotate(9deg) brightness(105%) contrast(107%)',
-      starHeight: '25px',
-      starCount: 5,
+      shadeColor: 'rates-yellow',
+      shapeHeight: '25px',
+      shapeCount: 5,
       shape: 'white-star',
+      imagesFolderLocation: '',
 
     }, options);
 
     return this.each(function () {
-      const $container = this;
+      const container = this;
+      $(container).addClass('rates-container');
       const $containerName = $(this).attr('id');
 
       const score = {
         value: 0,
       };
 
-      colorHueSetter();
-      createStars(this, settings.starCount);
+      createStars(settings.shapeCount);
       setSize();
 
       const $eachStar = $(this).find('img');
 
-      //Colors in the rating shape on hover 
-      //Removes the color from above the selected rating on mouse out
+      // Colors in the rating shape on hover
+      // Removes the color from above the selected rating on mouse out
       $(this).find('img').hover(function () {
         const starIndex = $eachStar.index(this);
-        $eachStar.css('filter', '');
-        for (let i = 0; i <= starIndex; i++) {
-          const star = $eachStar.get(i);
-          $(star).css('filter', settings.shadeColor);
-        }
+        colorShapesToIndex(starIndex);
       }, () => {
-        $eachStar.css('filter', '');
-        for (let j = 0; j < score.value; j++) {
-          const star = $eachStar.get(j);
-          $(star).css('filter', settings.shadeColor);
-        }
+        colorShapesToScore();
       });
 
-      //Sets the score rating based on which rating shape was clicked
+      // Sets the score rating based on which rating shape was clicked
       $(this).find('img').on('click', function () {
         const starIndex = $eachStar.index(this);
-        $eachStar.css('filter', '');
-        for (let i = 0; i <= starIndex; i++) {
-          const star = $eachStar.get(i);
-          $(star).css('filter', settings.shadeColor);
-        }
+        colorShapesToIndex(starIndex);
         score.value = starIndex + 1;
         $(`#${$containerName}Rating`).val(score.value);
       });
 
-      //Sets the size of stars indicated in the settings
-      function setSize(){
-        $($container).find('img').css('height', settings.starHeight)
+      // Sets the size of stars indicated in the settings
+      function setSize() {
+        $(container).find('img').css('height', settings.shapeHeight);
       }
 
-      //Dynamically creates the html markup based on the number of stars indicated
-      function createStars(container, count) {
-        const $starInput = $(`<input type="hidden" id = "${$containerName}Rating" name="${$containerName}Rating" value="0" >`);
-        $(container).append($starInput);
-        for (i = 0; i < count; i++) {
-          const $imageStar = $('<img class = starstyle >');
-          $imageStar.attr('src', `images\\${settings.shape}.png`);
+      // Dynamically creates the html markup based on the number of stars indicated
+      function createStars(count) {
+        const starInput = $(`<input type="hidden" id = "${$containerName}Rating" name="${$containerName}Rating" value="0" >`);
+        $(container).append(starInput);
+        for (let i = 0; i < count; i++) {
+          const $imageStar = $('<img>');
+          $imageStar.attr('src', `${settings.imagesFolderLocation}images/${settings.shape}.png`);
           $(container).append($imageStar);
         }
       }
 
-      //Sets the fill in color of the shape
-      function colorHueSetter() {
-        if (settings.shadeColor.toLowerCase() == 'red') {
-          settings.shadeColor = 'brightness(0) saturate(100%) invert(21%) sepia(70%) saturate(4945%) hue-rotate(352deg) brightness(87%) contrast(102%)';
-        } else if (settings.shadeColor.toLowerCase() == 'yellow') {
-          settings.shadeColor = 'brightness(0) saturate(100%) invert(89%) sepia(82%) saturate(5150%) hue-rotate(9deg) brightness(105%) contrast(107%)';
+      // Resets the shading class on the shapes to color only those up until a designated index
+      function colorShapesToIndex(starIndexValue) {
+        $eachStar.removeClass(settings.shadeColor);
+        for (let i = 0; i <= starIndexValue; i++) {
+          const star = $eachStar.get(i);
+          $(star).toggleClass(settings.shadeColor);
+        }
+      }
+
+      // Resets the shading class on the shapes to color only those up to and including the selected score
+      function colorShapesToScore() {
+        $eachStar.removeClass(settings.shadeColor);
+        for (let j = 0; j < score.value; j++) {
+          const star = $eachStar.get(j);
+          $(star).toggleClass(settings.shadeColor);
         }
       }
     });
